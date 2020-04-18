6e4573dd3057e679db9aa6016b1ff69c72266535
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-04-18 12:52:09 -0600

    Styling metric section

--- src/components/weeklyForcast.js
+++ src/components/weeklyForcast.js
@@ -5,20 +5,15 @@ import Weekday from './weekday';
 
 const WeeklyForcast = ({ week }) => {
 
-    // const weekday = week.list;
-    // console.log('Weekly Component', weeklyData)
-
-
-    // console.log("show",week.list)
+    // console.log("show week data",week.list)
 
 
     //TODO's:
-    //implement one of the react sliders. May not need if we are only doing 5 day. Need to figure out if we will get more or request for 7 day look out
     // overall will need to decide on layout when weather icon is on bigger screen sizes like tablets. Maybe limit it to those sizes and make it responsive down to phone screens. This may effect the sliders.
 
     return (
         <>
-            <Card className="weeklyForcast card">
+            <Card className="weeklyForcast p-1 card ">
             {week.list
                 ? week.list.map((day) => {
                     let time = sunTime(day.dt);
